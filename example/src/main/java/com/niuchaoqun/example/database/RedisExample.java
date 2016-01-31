b61800cb637c611dcae7eb24868325b611693bd1
ncq <ncq@ncqdeMacBook-Pro.local>
2016-01-31 23:17:19 +0800

    no message

--- example/src/main/java/com/niuchaoqun/example/database/RedisExample.java
+++ example/src/main/java/com/niuchaoqun/example/database/RedisExample.java
@@ -1,16 +1,40 @@
 package com.niuchaoqun.example.database;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisExample {
 	public static void run(String[] args) {
-		System.out.println("redis");
+		single();
+		pool();
 	}
-	
-	public static void RedisExample() {		
-		Jedis jedis = new Jedis("localhost", 6379);
+
+	public static void single() {
+		// A single Jedis instance is not threadsafe!
+		Jedis jedis = new Jedis("localhost");
 		jedis.set("foo", "bar");
 		String value = jedis.get("foo");
 		System.out.println(value);
+
+		System.out.println("=====");
+	}
+
+	public static void pool() {
+		JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
+		Jedis jedis = null;
+
+		try {
+			jedis = pool.getResource();
+			jedis.set("foo", "bar");
+			String foobar = jedis.get("foo");
+			System.out.println(foobar);
+		} finally {
+			if (jedis != null) {
+				jedis.close();
+			}
+		}
+
+		pool.destroy();
 	}
 }
