2fa8d1d91d9c1fd41ad4e2f4fe05e8c836a7781e
ncq <ncq@ncqdeMacBook-Pro.local>
2016-02-01 09:28:13 +0800

    init

--- example-hbase/src/main/java/com/niuchaoqun/example/hbase/Example.java
+++ example-hbase/src/main/java/com/niuchaoqun/example/hbase/Example.java
@@ -1,5 +1,5 @@
 package com.niuchaoqun.example.hbase;
 
-public class PutExample {
+public class Example {
 
 }
--- example/src/main/java/com/niuchaoqun/example/hbase/AdminExample.java
+++
@@ -1,7 +0,0 @@
-package com.niuchaoqun.example.hbase;
-
-public class AdminExample {
-	public AdminExample() {
-		// TODO Auto-generated constructor stub
-	}
-}
